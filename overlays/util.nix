143b5ae63c396cc4652e07707d6672b373c2ab4f
z3ji <z3ji@proton.me>
2024-03-30 16:48:31 +0000

    Update util.nix

--- overlays/util.nix
+++ overlays/util.nix
@@ -4,11 +4,11 @@ self: super: {
     dirEntries = path: builtins.attrNames (builtins.readDir path);
 
     # Function to map over hosts directories
-    mapHosts = f: builtins.map f (util.dirEntries ../hosts);
+    mapHosts = f: builtins.map f (dirEntries ../hosts);
 
     # Function to map over hosts attributes
     mapHostAttrs = key_f: val_f:
-      builtins.listToAttrs (util.mapHosts (host: {
+      builtins.listToAttrs (mapHosts (host: {
         name = key_f host;
         value = val_f host;
       }));
@@ -22,8 +22,8 @@ self: super: {
     # Function to filter Nix files in a directory and import them
     nixFilesIn = path:
       builtins.map
-      (e: import (path + "/${e}"))
-      (builtins.filter (x: (builtins.match ".*\\.nix$" x) != null) (util.dirEntries path));
+      (e: import (path + /${e}))
+      (builtins.filter (x: (builtins.match ".*\\.nix$" x) != null) (dirEntries path));
 
     # Function to filter out exclusions from a list
     without = exclusions: list: builtins.filter (x: !builtins.elem x exclusions) list;
